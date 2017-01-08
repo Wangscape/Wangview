911e8fb8f907cecac9fe6e624a5d6279c5f400a9
Serin Delaunay <serin.delaunay@gmail.com>
2017-01-08 20:55:59 +0000

    Finish documenting Hypergraph

--- Wangview/Hypergraph.py
+++ Wangview/Hypergraph.py
@@ -43,28 +43,80 @@ class Hypergraph(object):
         # Take the union of the remaining cliques
         return self.flatten_options(reduce(combine, seq, start))
     def terrain_options_2(self, t_left=[], t_up=[]):
+        """
+        Returns a frozenset containing the terrains which can legally be placed
+        in a location with adjacent terrains as specified in `t_left` and `t_up`.
+        
+        The following graph illustrates the meaning of `t_left` and `t_up`:
+        ```
+        | 1.| 2. | 3. | 4. | 5.  | 6. | 
+        |   |    |    | UV | UVW | UV | 
+        | O | LO | LO | O  | LO  | LO | 
+        ```
+        `O` represents the output terrain.
+        `L` represents the 0 or 1 terrains in `t_left`.
+        `U`, `V`, and `W` represent the 0, 2, or 3 terrains in `t_up`.
+        Cases 4, 5, and 6 are used when the output terrain must agree
+        with an adjacent line.
+        Cases 1, 2, and 3 are used otherwise.
+        Cases 1 and 4 are used when the output terrain is the first in the line.
+        There is no terrain to the left, so the only constraints come from U and V.
+        In case 1, U, V, and L are all missing, so there are no constraints.
+        Cases 2 and 5 are used when the output terrain is in the middle of the line.
+        In case 5, the output terrain is in the corner of two different tiles,
+        which impose separate constraints: UVL and VW.
+        Cases 3 and 6 are used when the output terrain is the last in the line.
+        With no tile to the right, the only constraints come from UVL, or just L.
+        See also: `terrain_options`, `generate_line`
+        """
         if len(t_up) == 0:
+            # Case 1, 2, or 3.
+            # Evaluate constraint L or {}
             return self.terrain_options(*t_left)
+        # Case 4, 5, or 6.
+        # Evaluate constraint L, UV, or UVL
+        # (depending on the lengths of t_left and t_up)
         x = self.terrain_options(*(t_left+t_up[:2]))
         if len(t_up) == 3:
+            # Case 5.
+            # Evaluate constraint VW
             y = self.terrain_options(*t_up[1:])
+            # The result is the intersection of constraints UVL and VW
             return x.intersection(y)
+        # Return the unaltered constraint L, UV, or UVL
         return x
     def generate_line(self, width, previous_line=None):
+        """
+        Generates a line of terrain values which satisfy adjacency constraints.
+        Each output terrain will agree with the adjacent terrains in the output line.
+        If previous_line is a sequence of terrains,
+        Each output terrain will also agree with the adjacent terrains in that line.
+        """
         new_line = []
+        # L in `terrain_options_2` is not present in the first row
         t_left = []
         t_up = []
         for i in range(width):
             if previous_line is not None:
+                # Try to get the slice of the previous line containing indices (i-1, i, i+1).
+                # These are U, V, L in `terrain_options_2`.
+                # Slice indices after the end are not a problem,
+                # But slice indices before the beginning must be replaced by 0.
                 t_up = previous_line[max(0,i-1):i+2]
+            # Get a container of possible terrains for this position
             options = self.terrain_options_2(t_left, t_up)
+            # Select a random terrain from the options
             new_line.append(random.choice(list(options)))
+            # Set L to the value just inserted
             t_left = [new_line[-1]]
         return new_line
     def generate_lines(self, width, height):
+        """Yields each line of a grid of terrain values satisfying the ajdacency constraints"""
+        # Yield a line with no constraints from a preceding line
         line = self.generate_line(width)
         yield line
         for i in range(height):
+            # Yield a line with additional constraints from the previous line
             line = self.generate_line(width, line)
             yield line
 
@@ -72,6 +124,7 @@ class Hypergraph(object):
 # In[ ]:
 
 if __name__ == '__main__':
+    # A quick test that the class is working correctly
     th = Hypergraph({'a':[['a','b'],['c','a']],
                      'b':[['a','b'],['b','c']],
                      'c':[['b','c'],['c','a']]})
