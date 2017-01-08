26abd2f5e8d5fca464e3e4759297c471ff073f82
Serin Delaunay <serin.delaunay@gmail.com>
2017-01-08 01:43:09 +0000

    Start documenting Display routines

--- Wangview/Display.py
+++ Wangview/Display.py
@@ -21,6 +21,7 @@ class Display(object):
                  fn_tile_groups='tile_groups.json',
                  fn_terrain_hypergraph='terrain_hypergraph.json',
                  fn_tileset_data='tilesets.json'):
+        # Initialise file path and metadata
         self.rel_path = rel_path
         with open(path.join(rel_path, fn_tileset_data),'r') as f:
             self.init_tilesets(json.load(f))
@@ -28,49 +29,86 @@ class Display(object):
             self.init_tile_groups(json.load(f))
         with open(path.join(rel_path, fn_terrain_hypergraph),'r') as f:
             self.hypergraph = Hypergraph(json.load(f))
+        # Initialise geometry info
         self.terminal_width = blt.state(blt.TK_WIDTH)
         self.terminal_height = blt.state(blt.TK_HEIGHT)
-        self.terrain_width = self.terminal_width+2
-        self.terrain_height = self.terminal_height+2
+        # Tiles may be offset while scrolling,
+        # so one extra row and column is required
         self.tile_width = self.terminal_width+1
         self.tile_height = self.terminal_height+1
+        # Terrain values are specified in the corners of the graphical tiles,
+        # so another extra row and column is required
+        self.terrain_width = self.terminal_width+2
+        self.terrain_height = self.terminal_height+2
+        # Select terrain values
         self.init_terrain_map()
+        # Select tile values based on terrain values
         self.init_tile_map()
     def simplify_tile(self, tile):
+        """
+        Converts a full specification of a tile's location in a tileset
+        into the unicode codepoint where it will have been loaded.
+        See also: `init_tilesets()`"""
         tileset = self.tilesets[tile['filename']]
         return (tileset.offset +
                 tileset.width*tile['y']//self.resolution +
                 tile['x']//self.resolution)
     def simplify_tile_group(self, tile_group):
+        """Converts every tile in a group into a unicode codepoint"""
         return [self.simplify_tile(tile) for tile in tile_group]
     def init_tile_groups(self, raw_groups):
+        """
+        Converts the data the `tile_groups.json` metadata file
+        into a format suitable for Wangview.
+        Example input:
+        {"g.g.g.g": [{"filename": "g.s.png", "x":0, "y":0},
+                     {"filename": "v.g.png", "x":96,"y":96}]}
+        output:
+        {("g", "g", "g", "g"): [0xe000, 0xe01f]}
+        """
         self.tile_groups = {tuple(k.split('.')):self.simplify_tile_group(v)
                             for (k,v) in raw_groups.items()}
     def init_tilesets(self, raw_tileset_data):
+        """
+        Converts the data in the `tilesets.json` metadata file
+        into a format suitable for Wangview,
+        and loads tilesets into bearlibterminal.
+        """
         first_tileset = True
         for tileset in raw_tileset_data:
+            # All tilesets specify tile resolution
             resolution = tileset['resolution']
             if first_tileset:
+                # Use the first tileset's resolution
                 self.resolution = resolution
+                # Initialise bearlibterminal
                 blt.open()
                 config_string = "window: size=30x20, cellsize={0}x{0}, title='Wangview'".format(
                     self.resolution)
                 blt.set(config_string)
+                # Start tile unicode blocks in private space
                 tileset_offset_counter = 0xE000
+                # Initialise converted metadata
                 self.tilesets = {}
+                # Only run this block once
                 first_tileset = False
+            # Validate remaining tilesets' resolutions
             assert(self.resolution == resolution)
+            # Calculate the number of tiles the tileset has in each axis
             rx = tileset['x']//self.resolution
             ry = tileset['y']//self.resolution
+            # Add the tileset's entry to the tileset container
             filename = tileset['filename']
             self.tilesets[filename] = Tileset(
                 filename, tileset_offset_counter,
                 rx,ry, tuple(tileset['terrains']))
+            # Load the tileset in bearlibterminal
             config_string = "0x{0:x}: {1}, size={2}x{2}".format(
                     tileset_offset_counter,
                     path.join(self.rel_path, filename),
                     self.resolution)
             blt.set(config_string)
+            # Insert the next tileset's tiles at the correct unicode codepoint
             tileset_offset_counter += rx*ry
     def init_terrain_map(self):
         terrain_iter = self.hypergraph.generate_lines(
