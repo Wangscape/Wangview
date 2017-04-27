6378b52a7e24b03b043e5649beaac51d914661f1
Serin Delaunay <serin.delaunay@gmail.com>
2017-04-27 17:20:10 +0100

    Add docstrings to FPSLimiter

--- Wangview/FPSLimiter.py
+++ Wangview/FPSLimiter.py
@@ -3,6 +3,9 @@
 from time import perf_counter, sleep
 
 class FPSLimiter:
+    """
+    Allows framerates to be throttled and measured.
+    """
     def __init__(self, max_fps = 60):
         self._frame_count = 0
         self._fps = 0
@@ -10,9 +13,16 @@ class FPSLimiter:
         self._previous_time = perf_counter()
         self._start_of_second = self._previous_time
     def set_max_fps(self, max_fps):
+        """
+        Changes the FPSLimiter's maximum framerate to max_fps.
+        """
         self._max_fps = max_fps
         self._interval = 1./max_fps
     def wait(self):
+        """
+        When called every frame, prevents the maximum framerate from being
+        exceeded by calling time.sleep() with an appropriate interval.
+        """
         t = perf_counter()
         self._frame_count += 1
         if t - self._start_of_second >= 1.:
@@ -26,4 +36,7 @@ class FPSLimiter:
             sleep(correct_sleep_interval)
         self._previous_time = perf_counter()
     def get_fps(self):
+        """
+        Returns the number of times wait() was called in the last completed second.
+        """
         return self._fps
