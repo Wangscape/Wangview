943b6850b24986b618861be093a98bc6f61145f2
Serin Delaunay <serin.delaunay@gmail.com>
2017-04-28 21:28:44 +0100

    OLD_PATH -> OLDPATH

--- package.bat
+++ package.bat
@@ -1,5 +1,5 @@
 echo Please ensure PYTHON is set to the directory of Python installation at version 3.4 or earlier.
-OLD_PATH=%PATH%
+OLDPATH=%PATH%
 PATH=%PYTHON%;%PYTHON%\Scripts;%PATH%
 python --version
 pip --version
@@ -10,4 +10,4 @@ python setup.py py2exe
 cd dist
 7z a Wangview.zip Wangview
 cd ..
-PATH=%OLD_PATH%
+PATH=%OLDPATH%
