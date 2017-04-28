e8a410a6cb29b2cfcdcc1df0b1e964206b1a1aab
Serin Delaunay <serin.delaunay@gmail.com>
2017-04-28 21:15:56 +0100

    Move packaging commands into a batch file
    Pending some changes in setup.py and appveyor.yml.

---
+++ package.bat
@@ -0,0 +1,12 @@
+OLD_PATH=%PATH%
+PATH=%PYTHON%;%PYTHON%\Scripts;%PATH%
+python --version
+pip --version
+pip install py2exe
+pip install -r requirements.txt
+mkdir dist
+python setup.py py2exe
+cd dist
+7z a Wangview.zip Wangview
+cd ..
+PATH=%OLD_PATH%
